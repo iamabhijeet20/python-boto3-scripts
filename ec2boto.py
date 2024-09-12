8340eda6e9d91cdfdf0a49ddbb915f011cddde0b
iamabhijeet20 <iamabhijeetbhovar@gmail.com>
2024-09-12 19:26:58 +0530

    fixed:more-efficient

--- ec2boto.py
+++ ec2boto.py
@@ -1,13 +1,13 @@
 import boto3
+from pprint import pprint
 
 aws_management = boto3.session.Session('default')
 
-client = boto3.client('ec2')
+ec2_client = boto3.client('ec2')
 
-list = client.describe_instances()
+list = ec2_client.describe_instances()['Reservations']
 
-for reservation in list['Reservations']:
-    for instance in reservation['Instances']:
-        if 'Tags' in instance:
-            for tag in instance['Tags']:
-                print(f"The Ec2 Instances Name(tag) is {tag['Value']}")
+for instance in list:
+    for tags in instance['Instances']:
+        for name in tags['Tags']:
+            print(f"The Ec2 Instances Name(tag) is {name['Value']}")
